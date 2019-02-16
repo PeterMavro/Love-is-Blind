dcc98a1790a539a72eb3d9e7d5dd0f28f6719e29
yannic2005 <yannic@toulousenet.de>
2019-02-16 20:40:11 +0100

    PauseMenu

---
+++ Love is Blind/Assets/Scripts/PauseMenu/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
