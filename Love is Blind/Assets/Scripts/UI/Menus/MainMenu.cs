f41aa1420d54b6f33c58f5c082719344bb958694
d4160 <d4160@outlook.com>
2019-02-23 02:55:31 -0500

    Main menu (incomplete)

---
+++ Love is Blind/Assets/Scripts/UI/Menus/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject howToPlay;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(1, LoadSceneMode.Single);
+    }
+
+    public void PlayButtonClickAudio()
+    {
+        AudioManager.Instance.Play("UI", "ButtonClick", AudioType.SFX);
+    }
+
+    public void PlayButtonHoverAudio()
+    {
+        AudioManager.Instance.Play("UI", "ButtonHover", AudioType.SFX);
+    }
+}
